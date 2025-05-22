e6025be2456dbb47637665bb5d97eb9e3e8000ad
Alexander <ex3me92@mail.ru>
2025-05-22 20:26:49 +0300

    Styling

--- app/models/scan_record.rb
+++ app/models/scan_record.rb
@@ -1,6 +1,8 @@
 # app/models/scan_record.rb
 class ScanRecord < ApplicationRecord
-validates :qr_code_id, uniqueness: true
+  validates :qr_code_id, uniqueness: true
+
+
 
   def parsed_qr_data
     return {} unless qr_code_text.present?
@@ -9,19 +11,24 @@ validates :qr_code_id, uniqueness: true
     return {} if parts.size < 7
 
     raw_sizes = parts[6].strip
-
     size_entries = raw_sizes.split(/\s*,\s*/)
+    total_quantity = 0
+    multiple_sizes = size_entries.size > 1
 
     parsed_sizes = size_entries.map do |entry|
-      # Удаляем "ед." и разбираем
       cleaned_entry = entry.strip.sub(" ед.", "")
 
       if cleaned_entry =~ /(.*?)-(\d+)\z/
         size = $1.strip
-        quantity = $2.strip
-        { size: size, quantity: quantity }
+        quantity = $2.strip.to_i
+        total_quantity += quantity
+        {
+          size: size,
+          quantity: quantity,
+          full_text: multiple_sizes ? "#{size} - #{quantity} ед." : size
+        }
       else
-        { size: cleaned_entry, quantity: "" }
+        { size: cleaned_entry, quantity: 0, full_text: cleaned_entry }
       end
     end
 
@@ -30,7 +37,8 @@ validates :qr_code_id, uniqueness: true
       apzgr: parts[2],
       box_number: parts[3],
       article: parts[4],
-      sizes_with_quantities: parsed_sizes
+      sizes_with_quantities: parsed_sizes,
+      total_quantity: total_quantity
     }
   end
 end
